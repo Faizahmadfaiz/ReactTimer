c8bd31bd7971534623dba0133046a244e5ec9fdb
FAIZ <faizahmadf@gmail.com>
2017-03-02 23:56:15 +0530

    Add Countdown funvtionality

--- app/tests/components/Clock.test.jsx
+++ app/tests/components/Clock.test.jsx
@@ -2,7 +2,6 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var expect = require('expect');
 var $ = require('jquery');
-
 var TestUtils = require('react-addons-test-utils');
 
 var Clock = require('Clock');
